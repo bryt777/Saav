0294ad6df724d18fc275b8ec48c2b544e57e6e70
Bryt Uchiha <brytser@gmail.com>
2026-06-19 16:21:39 +0530

    Add functions to set integer and float values in buffer

--- lib/writer.ts
+++ lib/writer.ts
@@ -1,8 +1,29 @@
-import { SaveData } from "./types";
+export function setInt(
+  buffer: Buffer,
+  key: string,
+  value: number
+) {
+  const idx = buffer.indexOf(key);
 
-export function rebuildSave(
-  original: Buffer,
-  data: SaveData
-): Buffer {
-  return original;
+  if (idx === -1) return;
+
+  buffer.writeInt32LE(
+    value,
+    idx + key.length + 29
+  );
+}
+
+export function setFloat(
+  buffer: Buffer,
+  key: string,
+  value: number
+) {
+  const idx = buffer.indexOf(key);
+
+  if (idx === -1) return;
+
+  buffer.writeFloatLE(
+    value,
+    idx + key.length + 33
+  );
 }
