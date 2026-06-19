485380815d9eff34a427c4510636ad3aa31b5253
Bryt Uchiha <brytser@gmail.com>
2026-06-19 16:24:16 +0530

    Add new settings interfaces for crosshair, audio, and sensitivity

--- lib/types.ts
+++ lib/types.ts
@@ -4,6 +4,25 @@ export interface GraphicsSettings {
   brightness: number;
 }
 
+export interface CrosshairSettings {
+  color: string;
+  hitEffectColor: string;
+}
+
+export interface AudioSettings {
+  mainVolume: number;
+  uiVolume: number;
+}
+
+export interface SensitivitySettings {
+  camera: number;
+  ads: number;
+  gyro: number;
+}
+
 export interface SaveData {
   graphics: GraphicsSettings;
+  crosshair: CrosshairSettings;
+  audio: AudioSettings;
+  sensitivity: SensitivitySettings;
 }
